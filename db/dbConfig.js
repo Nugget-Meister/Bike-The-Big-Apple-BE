f13a895534926d764c288cd91d698470d4b29dd2
Nugget-Meister <tremainecamacho@gmail.com>
2024-05-11 16:52:15 -0400

    use direct string for connection

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -13,8 +13,9 @@ const cn = {
 
 console.log(cn)
 
-const db = pgp(cn);
+// const db = pgp(cn);
+const db = pgp('postgres://btba_sql_user:qdzCpOiwXQaqXNEIUYakQtZa9PvcthfG@dpg-coin4m8l5elc73dafqf0-a/btba_sql')
 
-console.log(db)
+// console.log(db)
 
 module.exports = db
