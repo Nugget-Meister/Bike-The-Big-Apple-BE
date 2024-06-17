e9a3f6de087e8987b7d0f68cbe5b82ca8f6ab427
Nugget-Meister <tremainecamacho@gmail.com>
2024-06-16 23:13:06 -0400

    connection string & ssl enabled, code cleanup

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,21 +1,20 @@
 const pgp = require("pg-promise")();
 require("dotenv").config();
 
-
-const cn = {
+const cn = process.env.PG_URL ? {
+    connectionString: process.env.PG_URL,
+    max: 30,
+    ssl: true
+} : {
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
     user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD || null
+    // password: process.env.PG_PASSWORD || null
 };
 
+const db = pgp(cn);
 
-console.log(cn)
-
-// const db = pgp(cn);
-const db = pgp(process.env.PG_URL || cn)
-
-// console.log(db)
+console.log('postgres: ',cn)
 
 module.exports = db
